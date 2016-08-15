6e8ff3b5237a8ba380496a8330dba096253c143d
Shawn Sherwood <shawn.sherwood@gmail.com>
2016-08-14 20:24:16 -0400

    Initial commit

---
+++ kafka-consumer/src/main/java/org/undertree/BootKafkaConsumerApplication.java
@@ -0,0 +1,12 @@
+package org.undertree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootKafkaConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootKafkaConsumerApplication.class, args);
+    }
+}
---
+++ kafka-producer/src/main/java/org/undertree/BootKafkaProducerApplication.java
@@ -0,0 +1,12 @@
+package org.undertree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootKafkaProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootKafkaProducerApplication.class, args);
+    }
+}
